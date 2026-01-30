c6f2bcbfe427b859b69f9109ec1864ac0d7ad772
DrissaCAS <Drissa_cas@hotmail.com>
2026-01-29 23:02:55 -0300

    feat: implement initial home section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,38 @@
-
+import { Link } from "react-router-dom"
+import S from "./home.module.scss"
+import Doctor from "../../assets/doctor.png"
+import Icone from "../../assets/icon_estetoscopio.png"
 
 export default function Home() {
   return (
     <main>
-        <h2>teste</h2>
+        <section className={S.section1}>
+          <article>
+            <div className={S.label}>
+              <img src={Icone} alt="Icone do Estetoscópio" />
+              <p>Saúde para todos</p>
+            </div>
+            
+            <h1>Saúde e cuidado sem barreiras</h1>
+            <p>Um projeto dedicado a oferecer atendimento médico e odontológico gratuito para pessoas que mais precisam na nossa comunidade.</p>
+
+            <div className={S.buttonsHome}>
+              <Link to={'/voluntario'}>Seja Voluntário</Link>
+              <Link to={'/'}>Como Ajudar</Link>
+            </div>
+          </article>
+          <article>
+            <img src={Doctor} alt="Photo ilustrativa de um mádico." />
+          </article>
+        </section>
+
+        <section className={S.section2}>
+
+        </section>
+
+        <section className={S.section3}>
+
+        </section>
     </main>
   )
 }
